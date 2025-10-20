e77512d758c0fd0848edccd2d482af27cfa69d48
Antoine <antoine.bats@gmail.com>
2025-10-20 13:54:40 +0000

    Ex9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+int nombre;
+
+for (nombre = '0'; nombre <= '9'; nombre++)
+{
+putchar(nombre);
+putchar(',');
+putchar(' ');
+}
+
+putchar('\n');
+
+return (0);
+}
