5a3fa0e264de44d5ade02c5bf42b9ddedaafec4c
Antoine <antoine.bats@gmail.com>
2025-10-20 08:25:55 +0000

    Ex4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,6 +2,7 @@
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle"); /* Write the text */
+/* Write the text */
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
