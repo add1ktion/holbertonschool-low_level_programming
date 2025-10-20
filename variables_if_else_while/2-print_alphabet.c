cc025b0739645ff1e765afb841b176f25493236d
Antoine <antoine.bats@gmail.com>
2025-10-20 12:06:07 +0000

    Ex2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+char lettre;
+
+for (lettre = 'a'; lettre <= 'z'; lettre++)
+putchar(lettre);
+
+return (0);
+}
+  
