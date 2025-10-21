51704d80560455856dc97e69e5cf1409275cc266
Antoine <antoine.bats@gmail.com>
2025-10-21 12:20:02 +0000

    Ex3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - check si minuscule
+ * @c: carac to check ASCII.
+ * Return: always 1 if lower and 0 is not.
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
