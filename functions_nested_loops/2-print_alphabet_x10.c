be783fccd76249fadab97f249ae507ad1f09e48c
Antoine <antoine.bats@gmail.com>
2025-10-21 12:06:51 +0000

    Ex2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 #include "main.h"
 /**
-* print_alphabet - affiche l'alphabet
-*
-* Return: always 0.
-*/
+ * print_alphabet_x10 - affiche l'alphabet 10 fois
+ *
+ * Return: always 0.
+ */
 void print_alphabet_x10(void)
 {
 char c;
