114018f10e784b3878e6bee8881f3f1caefabeb8
Antoine <antoine.bats@gmail.com>
2025-10-21 12:57:57 +0000

    Ex4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * _isalpha - check if lower & upper
- * @c: carac to check ASCII.
+ * @c: char to check ASCII.
  * Return: always 1 if lower and 0 is not.
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 return (1);
