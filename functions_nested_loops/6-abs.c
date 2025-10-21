b7aa8465113154a11e6e950f5d6f0149406e5421
Antoine <antoine.bats@gmail.com>
2025-10-21 13:37:20 +0000

    Ex6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - efrefrefrefr
+ * @n: check is positive or negative
+ * Return: always 0 (success)
+ */
+int _abs(int n)
+{
+if (n < 0)
+return (-n);
+else
+return (n);
+}
