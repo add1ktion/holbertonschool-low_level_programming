84270c0c60c5abb778ba9bb21a63ba8b40bbecbb
root <root@ip-10-42-155-27.ec2.internal>
2025-10-21 12:01:14 +0000

    Ex10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_alphabet - affiche l'alphabet
+ *
+ * Return: always 0.
+ */
+void print_alphabet_x10(void)
+{
+char c;
+int multi;
+
+for (multi = 0; multi < 10; multi++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
