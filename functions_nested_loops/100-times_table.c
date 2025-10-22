c61dd31f4f796b4af489d8c2993de2dd5b3b25fa
root <root@ip-10-42-130-114.ec2.internal>
2025-10-22 13:43:11 +0000

    Ex100

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -10,40 +10,31 @@ int x, i, res;
 
 if (n < 0 || n > 15)
 return;
-
 for (x = 0; x <= n; x++)
 {
 for (i = 0; i <= n; i++)
 {
 res = x * i;
-
+if (i != 0)
+{
 if (res < 10)
-{
-if (i != 0)
-_putchar(' ');
-_putchar(res + '0');
-}
+_putchar(' '), _putchar(' '), _putchar(' ');
 else if (res < 100)
-{
-if (i != 0)
-_putchar(' ');
-_putchar((res / 10) + '0');
-_putchar((res % 10) + '0');
-}
+_putchar(' '), _putchar(' ');
 else
-{
-if (i != 0)
 _putchar(' ');
+}
+if (res < 10)
+_putchar(res + '0');
+else if (res < 100)
+_putchar((res / 10) + '0'), _putchar((res % 10) + '0');
+else
 _putchar((res / 100) + '0');
 _putchar((res / 10) % 10 + '0');
 _putchar((res % 10) + '0');
-}
 if (i != n)
-{
-_putchar(',');
-_putchar(' ');
+_putchar(','), _putchar(' ');
+}
 _putchar('\n');
-}
-}
 }
 }
