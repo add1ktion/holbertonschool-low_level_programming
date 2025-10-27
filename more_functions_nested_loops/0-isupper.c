3cc99d57a22c9d2b5731556de54fa14b783c6939
Antoine <antoine.bats@gmail.com>
2025-10-27 07:34:30 +0000

    Ex1

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isupper - check if uppercase
+* @c: character checked
+* Return: 1 if upper 0 if lower
+*/
+int _isupper(int c)
+{
+    if (c >= 'A' && c <= 'Z')
+    return (1);
+
+    else
+    return (0);
+}
