e482c55c1e74970df0b6e2f30fae742448256e48
Antoine <antoine.bats@gmail.com>
2025-10-27 08:47:54 +0000

    Ex7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,14 +8,19 @@ void print_diagonal(int n)
 {
 int backslash, space;
 
-if (n > 0)
+if (n <= 0)
 {
+_putchar('\n');
+return;
+
 for (backslash = 0; backslash < n; backslash++)
 {
 for (space = 0; space < backslash; space++)
+{
 _putchar(' ');
-_putchar('\\');
 }
+_putchar('\\');
 _putchar('\n');
+}
 }
 }
