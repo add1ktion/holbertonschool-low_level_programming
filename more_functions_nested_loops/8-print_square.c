cd1080ecc2bd42652c29ce8efacf7e5aa40dcb3f
Antoine <antoine.bats@gmail.com>
2025-10-27 09:20:42 +0000

    Ex8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,6 +1,23 @@
 #include "main.h"
 /**
-* _isdigit - check if digit
-* @c: character checked
-* Return: 1 if digit 0 if not
+* print_square - prints #
+* @size: size oof square
 */
+void print_square(int size)
+{
+int haut, large;
+
+if (size > 0)
+{
+for (haut = 0; haut < size; haut++)
+{
+for (large = 0; large < size; large++)
+_putchar('#');
+
+if (haut == size - 1)
+continue;
+_putchar('\n');
+}
+}
+_putchar('\n');
+}
