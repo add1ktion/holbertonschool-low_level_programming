7d6851b7a009c1ffc091ac8c7352a2eb852919fb
Antoine <antoine.bats@gmail.com>
2025-10-27 08:06:23 +0000

    Ex4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,16 +2,16 @@
 /**
 * print_most_numbers - print digit from 0 to 9 w/out 2 and 4
 *
-* Return: Always 0
+*
 */
 void print_most_numbers(void)
 {
 int i;
 
-for (i = O; i <= 9; i++)
+for (int i = 0; i <= 9; i++)
 {
 if (i != 2 && i != 4)
 _putchar(i + '0');
 }
-_putchar('\n')
+_putchar('\n');
 }
