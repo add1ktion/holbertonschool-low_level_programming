eec7b593f5ce4059b04f4ed1948d06f579d73703
Antoine <antoine.bats@gmail.com>
2025-10-27 08:24:54 +0000

    Ex6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,6 +1,17 @@
 #include "main.h"
 /**
-* _isdigit - check if digit
-* @c: character checked
-* Return: 1 if digit 0 if not
+* print_line - prints _
+* @n: numbers of _
+*
 */
+void print_line(int n)
+{
+int long;
+
+if (n > 0)
+{
+for (long = 0; long < n; long++)
+_putchar(_);
+}
+_putchar('\n');
+}
