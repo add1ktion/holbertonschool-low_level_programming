e35333fb674275bef187f681978bf861dd50a41d
Antoine <antoine.bats@gmail.com>
2025-10-27 09:56:20 +0000

    Ex10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,6 +1,25 @@
 #include "main.h"
 /**
-* _isdigit - check if digit
-* @c: character checked
-* Return: 1 if digit 0 if not
+* print_triangle - prints a triangle made of ' ' and #
+* @size: size of triangle
 */
+void print_triangle(int size)
+{
+int diese, space;
+
+if (size > 0)
+{
+for (diese = 1; diese <= size; diese++)
+{
+for (space = size - diese; space > 0; space++)
+_putchar(' ');
+for (space = 0; space < diese; space++)
+_putchar('#');
+if (diese == space)
+continue;
+
+_putchar('\n');
+}
+}
+_putchar('\n');
+}
