02ae65adb7c7bc7a50a6beb3c10968e7f0c768c4
Antoine <antoine.bats@gmail.com>
2025-10-27 07:38:41 +0000

    Ex1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isupper - check if uppercase
+* @c: character checked
+* Return: 1 if upper 0 if lower
+*/
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+return (1);
+
+else
+return (0);   
+}
