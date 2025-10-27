71c4ca7d39d2068cb5ea75cf8ee713261d023800
Antoine <antoine.bats@gmail.com>
2025-10-27 08:29:23 +0000

    Ex7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,6 +1,17 @@
 #include "main.h"
 /**
-* _isdigit - check if digit
-* @c: character checked
-* Return: 1 if digit 0 if not
+* print_diagonal - prints backslash
+* @n: numbers of backslash
+*
 */
+void print_diagonal(int n)
+{
+int backslash;
+
+if (n > 0)
+{
+for (backslash = 0; backslash < n; backslash++)
+_putchar('\\');
+}
+_putchar('\n');
+}
