6888025cd5f168437dcc7d7693b9cc99e77d3189
Antoine <antoine.bats@gmail.com>
2025-10-27 08:00:34 +0000

    Ex4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,17 @@
 #include "main.h"
 /**
-* _isdigit - check if digit
-* @c: character checked
-* Return: 1 if digit 0 if not
+* print_most_numbers - print digit from 0 to 9 w/out 2 and 4
+*
+* Return: Always 0
 */
+void print_most_numbers(void)
+{
+int i;
+
+for (i = O; i <= 9; i++)
+{
+if (i != 2 && i != 4)
+_putchar(i + '0');
+}
+return (0);
+}
