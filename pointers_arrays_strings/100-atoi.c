25cf772c666b499299a55fed01c7924d1487321f
Antoine <antoine.bats@gmail.com>
2025-10-28 14:17:39 +0000

    Ex100

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,8 +2,31 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* puts2 - Prints every other char
-* @str: String to cut
+* _atoi - Convert a string to an integer
+* @s: String to convert
 *
-* Return: void
+* Return: An integer
 */
+int _atoi(char *s)
+{
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+	int start = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign = sign * -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			start = 1;
+			result = result * 10 + (s[i] - '0');
+		}
+		else if (start)
+		break;
+		i++;
+	}
+	return (result * sign);
+}
