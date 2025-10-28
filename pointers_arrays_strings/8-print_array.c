1b0aacfc14df8356ae63623d611b3d7934d50064
Antoine <antoine.bats@gmail.com>
2025-10-28 11:27:52 +0000

    Ex8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -2,8 +2,22 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* puts2 - Prints every other char
-* @str: String to cut
-*
+* print_array - Prints the content of an array
+* @a: Array to print
+* @n: Number of char to print
 * Return: void
 */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
