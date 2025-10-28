7f984aed824c50c4dc38b9101befd2127a97e352
Antoine <antoine.bats@gmail.com>
2025-10-28 11:07:11 +0000

    Ex7

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+* puts2 - Prints every other char
+* @str: String to cut
+*
+* Return: void
+*/
---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+* puts_half - Prints last half of string
+* @str: String to half print
+*
+* Return: void
+*/
+void puts_half(char *str)
+{
+	char *ptr = str;
+	int length = 0;
+
+	while (*(ptr + length) != '\0')
+	{
+		length++;
+	}
+
+	ptr = ptr + (length + 1) / 2;
+
+	while (*ptr != '\0')
+	{
+		_putchar(*ptr);
+		ptr++;
+	}
+	_putchar('\n');
+}
