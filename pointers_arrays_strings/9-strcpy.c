7f672e357c796e8284109a5ce5362e9b5bb61ebb
Antoine <antoine.bats@gmail.com>
2025-10-28 13:43:10 +0000

    Ex9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -2,8 +2,20 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* puts2 - Prints every other char
-* @str: String to cut
-*
-* Return: void
+* _strcpy - Copy a string
+* @dest: Destination de la string
+* @src: Source de la string
+* Return: La string
 */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i = i + 1;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
