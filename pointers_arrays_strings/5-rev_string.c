f5db20d3536406f4b620426d46553fb4e2ae6db5
Antoine <antoine.bats@gmail.com>
2025-10-28 07:49:21 +0000

    Ex5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -9,19 +9,16 @@
 */
 void rev_string(char *s)
 {
-	int i = 0;
-	int j;
+	int i, len = 0;
 	char tmp;
 
-	while (s[j] != '\0')
-	{
-		j++;
-	}
+	while (s[len] != '\0')
+		len++;
 
-	for (i = 0; i < j / 2; i++)
+	for (i = 0; i < len / 2; i++)
 	{
 		tmp = s[i];
-		s[i] = s[j - i - 1];
-		s[j - i - 1] = tmp;
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = tmp;
 	}
 }
