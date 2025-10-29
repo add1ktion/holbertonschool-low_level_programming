7ab9b1db448ae3b3222a94f5d8e4d937e0c7c020
Antoine <antoine.bats@gmail.com>
2025-10-29 18:01:00 +0000

    Ex102

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -1,8 +1,17 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
 /**
-* leet - Leetify strings
-* @str: String to leetify
-* Return: Leetified string
-*/
+ * main - prints a[2] = 98, followed by a new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	*(p + 5) = 98;
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
