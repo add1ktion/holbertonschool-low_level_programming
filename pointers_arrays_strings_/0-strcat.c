b39f2624a03d94772e11db972c8370ce99de01b6
Antoine <antoine.bats@gmail.com>
2025-10-29 07:41:45 +0000

    Ex0

--- pointers_arrays_strings_/0-strcat.c
+++ pointers_arrays_strings_/0-strcat.c
@@ -2,9 +2,28 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* print_array - Prints the content of an array
-* @a: Array to print
-* @n: Number of char to print
-* Return: void
+* _strcat - Concatenates two strings
+* @dest: String
+* @src: String to add to dest
+* Return: dest
 */
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
