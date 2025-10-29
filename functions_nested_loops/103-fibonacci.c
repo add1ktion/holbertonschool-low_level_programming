8a4abe8ba6ea2836810954e497d6f324fc43f1a3
Antoine <antoine.bats@gmail.com>
2025-10-29 17:04:12 +0000

    Ex103

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -7,23 +7,22 @@
 */
 int main(void)
 {
-    int a = 1;
-    int b = 2;
-    int pair = 0;
-    int next;
+int a = 1;
+int b = 2;
+int pair = 0;
+int next;
 
-    while (b <= 4000000)
-    {
-        if (b % 2 == 0)
-        {
-            pair += b;
-        }
+while (b <= 4000000)
+{
+if (b % 2 == 0)
+{
+pair += b;
+}
 
-        next = a + b;
-        a = b;
-        b = next;
-    }
-
-    printf("%d\n", pair);
-    return 0;
+next = a + b;
+a = b;
+b = next;
+}
+printf("%d\n", pair);
+return (0);
 }
