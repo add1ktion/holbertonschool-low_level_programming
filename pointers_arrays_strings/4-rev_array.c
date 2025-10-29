2f0cf689650df1bcfde159c53536ef27dcced7fd
Antoine <antoine.bats@gmail.com>
2025-10-29 09:50:44 +0000

    Ex4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -7,4 +7,19 @@
 * @n: Number of char to print
 * Return: void
 */
-void reverse_array(int *a, int n);
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int j = n - 1;
+	int tmp;
+
+	while (i < j)
+	{
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+
+		i++;
+		j--;
+	}
+}
