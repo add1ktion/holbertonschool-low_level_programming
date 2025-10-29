2998b8cb8b2fabcf610b023f1805cbc58c553fd1
Antoine <antoine.bats@gmail.com>
2025-10-29 10:21:35 +0000

    Ex6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,9 +2,28 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* print_array - Prints the content of an array
-* @a: Array to print
-* @n: Number of char to print
-* Return: void
+* cap_string - Prints every 1st letter of a word as upper
+* @str: String to modify
+* Return: String with upper
 */
-char *cap_string(char *);
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - ('a' - 'A');
+		}
+		else if (!((str[i - 1] >= 'a' && str[i - 1] <= 'z') ||
+					(str[i - 1] >= 'A' && str[i - 1] <= 'Z') ||
+					(str[i - 1] >= '0' && str[i - 1] <= '9')) &&
+					str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - ('a' - 'A');
+		}
+		i++;
+	}
+	return (str);
+}
