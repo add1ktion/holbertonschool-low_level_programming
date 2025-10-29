efe93c88ef0c7e7f5723f11fb3f5706ec95d39af
Antoine <antoine.bats@gmail.com>
2025-10-29 08:04:57 +0000

    Ex1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -2,9 +2,29 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* print_array - Prints the content of an array
-* @a: Array to print
-* @n: Number of char to print
-* Return: void
+* _strncat - Concatenates two strings
+* @dest: String
+* @src: String to add to dest
+* @n: Bytes used
+* Return: dest
 */
-char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
