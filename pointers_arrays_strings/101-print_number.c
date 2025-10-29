1d05cb5390e7733bf62596462445e9e6ede5333c
Antoine <antoine.bats@gmail.com>
2025-10-29 18:12:13 +0000

    Ex101

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -1,12 +1,22 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
 /**
-* leet - Leetify strings
-* @str: String to leetify
-* Return: Leetified string
+* print_number - Prints a number
+* @n: The integer to be printed
+*
 */
 void print_number(int n)
 {
-	
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
 }
