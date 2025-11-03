befef7fdb66afb6f411954b333cdc80790aa9f45
Antoine <antoine.bats@gmail.com>
2025-11-03 12:37:02 +0000

    Ex100

--- pointers_arrays_strings/100-set_string.c
+++ pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-* _strcat - Concatenates two strings
-* @dest: String
-* @src: String to add to dest
-* Return: dest
-*/
+ * set_string - Sets the value of a pointer to a char
+ * @s: Pointer to the pointer that will point to the new string
+ * @to: Pointer to the string to assign
+ * Return: 0
+ */
 void set_string(char **s, char *to)
 {
-	
+	*s = to;
 }
