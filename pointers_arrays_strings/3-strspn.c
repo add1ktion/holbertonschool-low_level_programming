f077403ecb9abcdee71be92d902f2228fb7c9147
Antoine <antoine.bats@gmail.com>
2025-11-03 11:00:40 +0000

    Ex3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -4,7 +4,8 @@
  * @s: Pointer to the string to search
  * @accept: Pointer to the string containing accepted characters
  *
- * Return: Number of bytes in the initial segment of s which consist only of bytes from accept
+ * Return: Number of bytes in the initial segment of s
+ * which consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
@@ -12,20 +13,20 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int i, j;
 	int found;
 
-	for(i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		found = 0;
 
-		for(j = 0; accept[j] != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if(s[i] == accept[j])
+			if (s[i] == accept[j])
 			{
 				found = 1;
 				break;
 			}
 		}
 
-		if(found == 0)
+		if (found == 0)
 			break;
 		count++;
 	}
