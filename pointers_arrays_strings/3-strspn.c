35ccec7d9760c82162b1d23cbec6d97d2aa47436
Antoine <antoine.bats@gmail.com>
2025-11-03 10:30:37 +0000

    Ex3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -8,14 +8,15 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int count = 0;
-	int i, j, found;
+	unsigned int count = 0;
+	unsigned int i, j;
+	int found;
 
 	for(i = 0; s[i] != '\0'; i++)
 	{
 		found = 0;
 
-		for(j = 0; accept[j]; j++)
+		for(j = 0; accept[j] != '\0'; j++)
 		{
 			if(s[i] == accept[j])
 			{
@@ -25,13 +26,8 @@ unsigned int _strspn(char *s, char *accept)
 		}
 
 		if(found == 0)
-		{
 			break;
-		}
-		else
-		{
-			count++;
-		}
+		count++;
 	}
 	return (count);
 }
