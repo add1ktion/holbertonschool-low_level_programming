87b9d8f645fc03a661ca7c6e7011bb69f2207cc0
Antoine <antoine.bats@gmail.com>
2025-11-03 11:00:27 +0000

    Ex0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
-* _memset - Fills memory with constant byte
-* @s: String
-* @b: String to add to dest
-* @n: dffs
-* Return: dest
-*/
+ * _memset - Fills memory with a constant byte
+ * @s: Pointer to the memory area to fill
+ * @b: The byte value to set
+ * @n: Number of bytes to fill
+ *
+ * Return: Pointer to the memory area s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
