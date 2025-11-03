0b04245290be1ee57e89d3d4ba3fe51d079445e0
Antoine <antoine.bats@gmail.com>
2025-11-03 08:09:11 +0000

    Ex0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,11 +1,21 @@
 #include "main.h"
 /**
-* _strcat - Concatenates two strings
-* @dest: String
-* @src: String to add to dest
+* _memset - Fills memory with constant byte
+* @s: String
+* @b: String to add to dest
+* @n: dffs
 * Return: dest
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-	
+	unsigned int i = 0;
+	unsigned char *memory = s;
+
+	while (n > 0)
+	{
+		*memory = b;
+		memory++;
+		n--;
+	}
+	return (s);
 }
