c374d87b299ed99a571f57bf326e4cd4e81e7355
Antoine <antoine.bats@gmail.com>
2025-11-04 12:21:56 +0000

    Ex1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 /**
-*
-*
-*
-*/
+ * main - Prints the number of arguments and then each argument
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	printf("%d\n", argc);
+	for (i = 0; i < argc - 1; i++)
+	{
+		printf("%s", argv[i]);
+	}
+	return (0);
+}
