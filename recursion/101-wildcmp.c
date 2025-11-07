3a8211a3e6448a5fdf3f93ab9c03eeff9809aa5f
Antoine <antoine.bats@gmail.com>
2025-11-07 14:05:52 +0000

    Ex101 v2

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -9,32 +9,34 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
+	int i = 0;
+
+	if (s1[i] == '\0' && s2[i] == '\0')
 	{
 		return (1);
 	}
 
-	if (*s2 == '*')
+	if (s2[i] == '*')
 	{
-		if ((*s2 + 1) == '*')
+		if ((s2[i + 1]) == '*')
 		{
-			return (wildcmp(s1, s2 + 1));
+			return (wildcmp(s1, &s2[i + 1]));
 		}
 
-		if (wildcmp(s1, s2 + 1))
+		if (wildcmp(s1, &s2[i + 1]))
 		{
 			return (1);
 		}
 
-		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+		if (s1[i] != '\0' && wildcmp(&s1[i + 1], s2))
 		{
 			return (1);
 		}
 	}
 
-	if (*s1 == *s2 && *s1 != '\0')
+	if (s1[i] == s2[i] && s1[i] != '\0')
 	{
-		return (wildcmp(s1 + 1, s2 + 1));
+		return (wildcmp(&s1[i + 1], &s2[i + 1]));
 	}
 
 	return (0);
