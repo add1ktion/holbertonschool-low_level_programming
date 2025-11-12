23ab49839cd6f271deeb5b38ede0447e404f0547
Antoine <antoine.bats@gmail.com>
2025-11-12 10:47:59 +0000

    Ex4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+#include "main.h"
+
+int _strlen(char *s);
+char *_strncpy(char *dest, char *src, int n);
+dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+* new_dog - Create a new dog
+* @name: Name of the new dog
+* @age: Age of the new dog
+* @owner: Owner of the new dog
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (dog_t == NULL)
+		return (NULL);
+
+	
+}
