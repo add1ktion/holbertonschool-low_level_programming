8765dbfdbf961845584ba6060c1f110de5b911ca
Antoine <antoine.bats@gmail.com>
2025-11-12 10:11:36 +0000

    Ex0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,16 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
 
-/** struct dog - Type with dogs informations
+/**
+* struct dog - Type with dogs informations
 * @name: name of the dog
 * @age: age of the dog
 * @owner: owner of the dog
 */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 } dog;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
