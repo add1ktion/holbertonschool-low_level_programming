0365cabc953b333de0472a7e0faad9648b9e44dc
Antoine <antoine.bats@gmail.com>
2025-11-12 12:46:23 +0000

    Ex4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -7,6 +7,41 @@ char *_strncpy(char *dest, char *src, int n);
 dog_t *new_dog(char *name, float age, char *owner);
 
 /**
+* _strlen - lenght of a string
+* @s: string to check
+* Return: lenght of @s
+*/
+int _strlen(char *s)
+{
+int count = 0;
+
+for (; *s != '\0'; s++)
+{
+count++;
+}
+return (count);
+}
+
+/**
+* _strcpy - Copy a string
+* @dest: Destination de la string
+* @src: Source de la string
+* Return: La string
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i = i + 1;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
 * new_dog - Create a new dog
 * @name: Name of the new dog
 * @age: Age of the new dog
@@ -14,8 +49,28 @@ dog_t *new_dog(char *name, float age, char *owner);
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (dog_t == NULL)
-		return (NULL);
+	dog_t *pug;
 
-	
+	pug = malloc(sizeof(dog_t));
+		if (pug == NULL)
+			return (NULL);
+
+	pug->name = malloc(sizeof(char) * (_strlen(name) + 1));
+		if (pug->name == NULL)
+		{
+			free(pug);
+			return (NULL);
+		}
+
+	pug->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+		if (pug->owner == NULL)
+		{
+			free(pug->name);
+			free(pug);
+			return (NULL);
+		}
+
+
+
+	return (pug);
 }
