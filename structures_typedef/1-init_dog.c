173e8c3d6a114357111d8ce04b58e69f7fc8d2b4
Antoine <antoine.bats@gmail.com>
2025-11-12 10:06:31 +0000

    Ex1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include "dog.h"
-/** init_dog - 
-* @d:
-* @name:
-* @age:
-* @owner:
+/** init_dog -
+* @d: Dog
+* @name: Name of dog
+* @age: Age of dog
+* @owner: Owner of dog
+* Return:
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	struct dog;
 
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	return (dog);
 }
