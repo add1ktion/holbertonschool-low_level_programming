91fdad76552928cd393988d07a8a813699c38228
Antoine <antoine.bats@gmail.com>
2025-11-13 07:31:14 +0000

    Ex5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+* free_dog - Frees a dog
+* @d: Dog to be freed
+*/
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
