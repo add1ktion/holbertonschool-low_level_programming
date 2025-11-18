20b8b37e3e86903fce904603697d3b16dc8f7d83
Antoine <antoine.bats@gmail.com>
2025-11-18 07:53:05 +0000

    Ex3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -21,22 +21,20 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[3]);
 	f = get_op_func(argv[2]);
 
-
 	if (f == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[2][1] == '\0')
+	if (((argv[2][0] == '/' && argv[2][1] == '\0') ||
+		(argv[2][0] == '%' && argv[2][1] == '\0')) &&
+		num2 == 0)
 	{
-		if (num2 == 0)
-		{
-			printf("Error\n");
-			exit(100);
-		}
+		printf("Error\n");
+		exit(100);
 	}
-	
+
 	result = f(num1, num2);
 	printf("%d\n", result);
 
