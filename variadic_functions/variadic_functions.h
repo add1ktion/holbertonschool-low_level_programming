7f3a046681449d6ba7187d32dbdfb82b5928d193
Antoine <antoine.bats@gmail.com>
2025-11-19 14:07:54 +0000

    Ex3

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,27 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
+/**
+ * struct type_txt - Struct linking format char to print function
+ * @type: format character (c, i, f, s)
+ * @print: function that prints the matching type
+ */
+typedef struct type_txt
+{
+	char type;
+	void (*print)(va_list);
+} type_txt_t;
+
+void print_char(va_list list);
+void print_int(va_list list);
+void print_float(va_list list);
+void print_string(va_list list);
+void print_all(const char * const format, ...);
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
 
 #endif
