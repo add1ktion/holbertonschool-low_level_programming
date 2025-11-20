3706c8c11a40885448f54405b2177d06102534b1
add1ktion <antoine.bats@gmail.com>
2025-11-20 22:07:27 +0100

    Ex100

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,15 +1,25 @@
+; 100-hello_world.asm
+; Assemble (Linux x86_64):
+;   nasm -f elf64 100-hello_world.asm -o 100-hello_world.o
+;   ld 100-hello_world.o -o 100-hello_world
+; Then run: ./100-hello_world
+
 section .data
-	text db "Hello, World", 10
+msg:    db "Hello, World", 10
+len:    equ $-msg
 
 section .text
-global main
-main:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, text
-    mov rdx, 14
-    syscall
+global _start
 
-    mov rax, 60
-    mov rdi, 0
-    syscall
+_start:
+	; write(1, msg, len)
+	mov     rax, 1      ; syscall: sys_write
+	mov     rdi, 1      ; file descriptor: stdout
+	mov     rsi, msg
+	mov     rdx, len
+	syscall
+
+	; exit(0)
+	mov     rax, 60     ; syscall: sys_exit
+	xor     rdi, rdi
+	syscall
