69f622ead25874dd8fc67a81b9099e840c5824a4
Antoine <antoine.bats@gmail.com>
2025-11-20 13:48:16 +0000

    Ex1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 /**
-* print_numbers - prints numbers separated by a string
-* @separator: string to print between integers
-* @n: number of integers passed
+* print_numbers - Prints numbers separated by a string.
+* @separator: String to print between integers.
+* @n: Number of integers passed.
 *
-* Return: Nothing
+* Description: Prints numbers separated by a string.
 */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
