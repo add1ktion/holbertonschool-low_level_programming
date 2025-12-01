2c25e63c9bf98693dd33c6145a22c79a2531abd9
Antoine <antoine.bats@gmail.com>
2025-12-01 10:39:55 +0000

    Ex0

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,6 +2,14 @@
 #define LISTS_H
 
 #include <stdlib.h>
+#include <stdio.h>
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
