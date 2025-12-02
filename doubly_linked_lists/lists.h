e3efafa166c9edcbb5d0e06253eccc35f49559e3
Antoine <antoine.bats@gmail.com>
2025-12-02 12:39:26 +0000

    Ex0

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -6,23 +6,17 @@
 #include <string.h>
 
 /**
- * struct list_s - Linked list
- * @str: String.
- * @len: Length of the string.
+ * struct listint_s - Double linked list
+ * @n: Interger.
+ * @prev: Points to previous node.
  * @next: Points to the next node.
  */
-typedef struct list_s
+typedef struct dlistint_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
-
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
