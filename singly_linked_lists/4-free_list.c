2a103b511162f2b9777e8833aeeac977f0e10335
Antoine <antoine.bats@gmail.com>
2025-12-02 10:24:54 +0000

    Ex4

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,5 +1,17 @@
 #include "lists.h"
+/**
+* free_list - Frees a list_t list.
+* @head: A pointer to the list_t list.
+*/
 void free_list(list_t *head)
 {
+	list_t *actual;
 
+	while (head != NULL)
+	{
+		actual = head->next;
+		free(head->str);
+		free(head);
+		head = actual;
+	}
 }
