a60de67cbd77e566c7d4c4b088bce0bfa71c8599
Antoine <antoine.bats@gmail.com>
2025-12-03 09:07:45 +0000

    Ex5

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	
+	if (head == NULL)
+        return (NULL);
+
+	if (index == 0)
+		return (head);
+
+	return get_dnodeint_at_index(head->next, index - 1);
 }
