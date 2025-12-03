4118e7fea2a17766c3ae255fad63f6e06760c587
Antoine <antoine.bats@gmail.com>
2025-12-03 09:28:01 +0000

    Ex4

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,4 +1,10 @@
 #include "lists.h"
+/**
+ * free_dlistint - Frees a dlistint_t doubly linked list
+ * @head: Pointer to the head of the linked list
+ *
+ * Return: void
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *actual;
