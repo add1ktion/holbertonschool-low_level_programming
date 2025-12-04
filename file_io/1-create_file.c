6bbf7a8d281908977a7590b95cd9cec45bc27388
Antoine <antoine.bats@gmail.com>
2025-12-04 11:17:59 +0000

    Ex1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+* create_file - Creates a file and writes text to it
+* @filename: Name of the file to create
+* @text_content: NULL-terminated string to write into the file
+*
+* Return: 1 on success, -1 on failure (cannot create or write to file)
+*/
 int create_file(const char *filename, char *text_content)
 {
 	int fd, n, len;
