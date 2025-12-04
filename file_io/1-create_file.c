015bc2cf8d48530d2058e650df953ad9096ba9a8
Antoine <antoine.bats@gmail.com>
2025-12-04 11:10:51 +0000

    Ex1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -2,5 +2,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
+	int fd, n, len;
 
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+		n = write(fd, text_content, len);
+		if (n != len)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
 }
