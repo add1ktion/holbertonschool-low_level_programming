7cdbda747f6aee4cc54bf6a07b46747f8e0f2933
Antoine <antoine.bats@gmail.com>
2025-12-05 08:13:22 +0000

    Ex2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -12,26 +12,27 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, n, len;
+	int fd, n;
 
 	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
-	{
-		close(fd);
 		return (1);
-	}
 
 	if (text_content == NULL)
+	{
+		close(fd);
 		return (-1);
+	}
 
-	len = strlen(text_content);
-
-	n = write(fd, text_content, len);
-	if (n != len)
+	n = write(fd, text_content, strlen(text_content));
+	if (n == -1)
+	{
+		close(fd);
 		return (-1);
+	}
 
 	close(fd);
 
